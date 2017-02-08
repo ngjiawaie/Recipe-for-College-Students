e12296352967cef815e3535a0df484b2fed0bf7f
Darryl Cheng Lin Wei <darrylclw@Gmail.com>
2017-02-08 23:12:11 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple Recipies</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="logo">Recipe For All</div>
+		<div class="right">
+			<div class="search">
+				<form>
+					<input type="search" name="query" placeholder="Search..." />
+				</form>
+			</div>
+		</div>
+	</header>
+	<div class="content-wrapper">
+		<h3>Recommendations: </h3>
+		<div class="content">
+			<div class="recipe">
+				<div class="foodimg">
+					<img src="https://i.guim.co.uk/img/media/8312a34a2f20b6f5127a6f8d73f9ff12c1280475/546_1645_3832_2298/master/3832.jpg?w=620&q=55&auto=format&usm=12&fit=max&s=7bc4d83ac4a097e10d32b5d42dfa65cb">
+				</div>
+				<div class="recipeDetails">
+					<h2>Some basic details here </h2>
+					<form>
+						<input type="submit" name="recipe" value="View Details"></button>
+					</form>
+				</div>
+			</div>
+			<div class="recipe">
+				<div class="foodimg">
+					<img src="https://i.guim.co.uk/img/media/8312a34a2f20b6f5127a6f8d73f9ff12c1280475/546_1645_3832_2298/master/3832.jpg?w=620&q=55&auto=format&usm=12&fit=max&s=7bc4d83ac4a097e10d32b5d42dfa65cb">
+				</div>
+				<div class="recipeDetails">
+					<h2>Some basic details here </h2>
+					<form>
+						<input type="submit" name="recipe" value="View Details"></button>
+					</form>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</body>
+</html>
