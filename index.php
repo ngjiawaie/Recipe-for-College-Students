d5f0d87611213bdff82cfd72fc834c59c887889b
Ng Jia Waie <ngjiawaie@users.noreply.github.com>
2017-02-08 23:54:16 +0800

    auto insert .sql file

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
 
 
-
+	$filename = 'recipes.sql';
 	$servername = "localhost";
-	$database = "college_recipes";
+	$database = "test";
 	$username = "root";
 	$password = "";
 
 	$conn = new mysqli($servername, $username, $password, $database)or die("connection problems");
+	mysql_select_db($database) or die('Error selecting MySQL database: ' . mysql_error());
 
 
+	// Temporary variable, used to store current query
+	$templine = '';
+	// Read in entire file
+	$lines = file($filename);
+	// Loop through each line
+	foreach ($lines as $line)
+	{
+	// Skip it if it's a comment
+	if (substr($line, 0, 2) == '--' || $line == '')
+	    continue;
+
+	// Add this line to the current segment
+	$templine .= $line;
+	// If it has a semicolon at the end, it's the end of the query
+	if (substr(trim($line), -1, 1) == ';')
+	{
+	    // Perform the query
+	    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+	    // Reset temp variable to empty
+	    $templine = '';
+	}
+	}
 
 	//get search query from search box
 	$select = $_POST["query"];
