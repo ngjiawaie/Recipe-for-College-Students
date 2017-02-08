88fde0eae888d1ca53e50db120b3e2a9a869c52c
Ng Jia Waie <ngjiawaie@users.noreply.github.com>
2017-02-08 23:26:29 +0800

    initial commit

--- index.php
+++ index.php
@@ -1,48 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Simple Recipies</title>
-	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<header>
-		<div class="logo">Recipe For All</div>
-		<div class="right">
-			<div class="search">
-				<form>
-					<input type="search" name="query" placeholder="Search..." />
-				</form>
-			</div>
-		</div>
-	</header>
-	<div class="content-wrapper">
-		<h3>Recommendations: </h3>
-		<div class="content">
-			<div class="recipe">
-				<div class="foodimg">
-					<img src="https://i.guim.co.uk/img/media/8312a34a2f20b6f5127a6f8d73f9ff12c1280475/546_1645_3832_2298/master/3832.jpg?w=620&q=55&auto=format&usm=12&fit=max&s=7bc4d83ac4a097e10d32b5d42dfa65cb">
-				</div>
-				<div class="recipeDetails">
-					<h2>Some basic details here </h2>
-					<form>
-						<input type="submit" name="recipe" value="View Details"></button>
-					</form>
-				</div>
-			</div>
-			<div class="recipe">
-				<div class="foodimg">
-					<img src="https://i.guim.co.uk/img/media/8312a34a2f20b6f5127a6f8d73f9ff12c1280475/546_1645_3832_2298/master/3832.jpg?w=620&q=55&auto=format&usm=12&fit=max&s=7bc4d83ac4a097e10d32b5d42dfa65cb">
-				</div>
-				<div class="recipeDetails">
-					<h2>Some basic details here </h2>
-					<form>
-						<input type="submit" name="recipe" value="View Details"></button>
-					</form>
-				</div>
-			</div>
+<?php
 
-		</div>
-	</div>
-</body>
-</html>
+
+
+	$servername = "localhost";
+	$database = "college_recipes";
+	$username = "root";
+	$password = "";
+
+	$conn = new mysqli($servername, $username, $password, $database)or die("connection problems");
+
+
+
+	//get search query from search box
+	$select = $_POST["query"];
+	$sql = "SELECT NAME,TYPE,CALORIES,INGREDIENT,VEGETARIAN,DIFFICULTY,SERVING,DURATION,METHOD FROM recipes WHERE NAME like '%".$select."%'";
+
+	$hasil = $conn->query($sql);
+	
+	while($result = mysqli_fetch_array($hasil)){
+	  echo "Name: " . $result["NAME"]. "<br><br> Type: " . $result["TYPE"]. "<br><br> Calories: ". $result["CALORIES"]. "<br><br> Ingredients: ". $result["INGREDIENT"]. "<br><br> Vegetarian: ". $result["VEGETARIAN"]."<br><br> Difficulty: ". $result["DIFFICULTY"]."<br><br> Serving: ". $result["SERVING"]."<br><br> Duration: ". $result["DURATION"]."<br><br> Method: ". $result["METHOD"]."<br><br>";
+	}
+
+	
+
+	
+?>
